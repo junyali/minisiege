9534699fb17a5cca01399053f8dd335a5b18b861
Junya <junjia.liu@pm.me>
2025-09-06 16:20:33 +0100

    Design main page

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import "./style.css";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-    <div></div>
+    <div class="min-h-screen bg-cover bg-center bg-fixed bg-no-repeat flex flex-col items-center justify-between p-8">
+        <div class="flex-shrink-0 mt-8">
+            <img src="/minisiege.png" alt="Minisiege" class="w-full max-w-[400px] sm:max-w-sm h-auto"/>
+        </div>
+        <div class="flex-1 flex items-center justify-center">
+            <button class="bg-[#7B0907] border-4 border-[#5A0605] text-white px-12 sm:px-24 py-3 sm:py-4 text-2xl font-bold rounded-2xl shadow-lg hover:transform hover:-translate-y-1 transition-transform duration-200 cursor-pointer" onclick="window.location.href='/game'">
+                Play
+            </button>
+        </div>
+        <footer class="flex-shrink-0 text-white text-center p-4">
+            meow
+		</footer>
+    </div>
 `
