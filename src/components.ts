1e6bf041f16b247862fda74f09906f6c4bd185c4
Junya <junjia.liu@pm.me>
2025-09-07 22:42:02 +0100

    Make footer consistent across pages

---
+++ src/components.ts
@@ -0,0 +1,12 @@
+export function createFooter(): string {
+	return `
+		<div class="w-full flex flex-col items-center mt-auto pt-2">
+            <div class="text-gray-950 text-lg tracking-widest text-center">
+                --------------------------------------------------------------------------------
+			</div>
+            <footer class="text-gray-950 text-2xl font-black text-center p-4">
+				made with ❤️ by <a href="https://github.com/junyali" class="text-[#7B0907] hover:underline transition-all duration-200">junya</a>!
+			</footer>
+		</div>
+	`;
+}
--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,5 @@
 import "./style.css";
+import { createFooter } from "./components.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="min-h-screen bg-cover bg-center bg-fixed bg-no-repeat flex flex-col items-center justify-between p-8">
@@ -47,13 +48,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 				</div>
 			</div>
 		</div>
-        <div class="w-full flex flex-col items-center mt-auto pt-2">
-            <div class="text-gray-950 text-lg tracking-widest text-center">
-                --------------------------------------------------------------------------------
-			</div>
-            <footer class="text-gray-950 text-2xl font-black text-center p-4">
-				made with ❤️ by <a href="https://github.com/junyali" class="text-[#7B0907] hover:underline transition-all duration-200">junya</a>!
-			</footer>
-		</div>
+        ${createFooter()}
     </div>
-`
+`;
